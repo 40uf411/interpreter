f1ff1fb153849a81d6706b8d8b30b180ab689214
Aouf Ali <ali25ab@outlook.com>
2018-12-27 21:07:44 +0100

    Update LexAna.py

--- LexAna.py
+++ LexAna.py
@@ -1,29 +1,38 @@
 import re
 
+
 class LexicalAnalyser(object):
     patterns = {}
     data = []
     
-    def __init__(self,patterns):
+    def __init__(self, patterns):
         self.patterns = patterns
     
-    def match(self, string):
+    def match(self, word):
+        found = False
         for pattern, value in self.patterns.items():
-            if re.compile(value).fullmatch(string):
-                self.data.append( (string,pattern) )
+            if re.compile(value).fullmatch(word):
+                found = True
+                if pattern != "ignore":
+                    self.data.append((pattern, word))
                 break
 
-    def parse(self, string):
+        #if not found:
+        #    raise Exception("Unknown word : '" + word + "'")
+
+    def parse(self, text):
         str_mode = False
         readed_word = ""
-        for letter in string:
-            if (not str_mode and letter in self.patterns['ignored'] ):
-                self.match(string = readed_word)
+        for letter in text:
+            if not str_mode and letter in self.patterns['breakpoint']:
+                self.match(word=readed_word)
+                self.match(word=letter)
                 readed_word = ""
             else:
-                readed_word += letter 
-                if (letter == '"'):
+                readed_word += letter
+                if letter == '"':
                     str_mode != str_mode
+        if readed_word is not None:
+            self.match(word=readed_word)
 
-        if readed_word != None:
-            self.match(string = readed_word)
+        return self.data
