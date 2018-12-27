02ee2ea6a8922fd80cadf906a19691e1fb54bea3
Aouf Ali <ali25ab@outlook.com>
2018-12-27 21:08:11 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,37 @@
 from LexAna import LexicalAnalyser
+from SynAna import SyntacticalAnalyser
 
-la = LexicalAnalyser({
+# our basic language words RE
+Lexics = {
     "token"      : r"if|then|else|for|fun|string|number|var",
-    "ignored"    : r"[\ |(|;]",
-    "literal"    : r"[+|-|*|/|=|==|!=|>|>=|<|<=|(|)|,|;]",
-    "string"     : r'\".*?\"|\'.*?\'[;]*',
-    "number"     : r'[0-9]+[.]{0,1}[0-9]*[;]*',
-    "identefire" : r'[a-zA-Z_][a-zA-Z0-9_]*[;]*',
-})
+    "ignore"     : r"[ ]",
+    "literal"    : r"[=|==|!=|>|>=|<|<=|(|)|,]",
+    "operator"   : r"[+|-|*|/]",
+    "string"     : r'\".*?\"|\'.*?\'',
+    "number"     : r'[0-9]+[.]{0,1}[0-9]*',
+    "identifier" : r'[a-zA-Z_][a-zA-Z0-9_]*',
+    # setting break points
+    "breakpoint" : "[ |;|+|-|*|/|=|==|!=|>|>=|<|<=|(|)|,|\n]",
+}
 
-la.parse(r"if (5>10) then print (5) ; alexander = 15")
+# our basic rules
+Syntax = {
+    "affectation" : [("identifier", ""), ("literal", "="), ("number", "")]
+}
 
-print(LexicalAnalyser.data)
+# our code
+code = "x = (a+b);"
+
+# creating our analysers
+la = LexicalAnalyser(Lexics)
+sa = SyntacticalAnalyser(Syntax)
+
+# processing the data
+
+l_data = la.parse(code)
+#s_data = sa.parse(l_data)
+
+# printing the results
+print("income code: ", code)
+print("lexical array : ", l_data)
+#print("syntactic tree", s_data)
